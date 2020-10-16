a90afb8ef6ab03f51b755fe7161bfebaa3c26537
vikei <vikei@gmail.com>
2020-10-16 15:10:07 +0300

    Add type-graphql & apollo-server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Ready to start...");
+import {ApolloServer} from "apollo-server";
+import "reflect-metadata";
+import {buildSchema} from "type-graphql";
+import {Container} from "typedi";
+
+const PORT = process.env.PORT ?? 4000;
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [__dirname + "/**/*.resolver.js"],
+    container: Container,
+  });
+  const server = new ApolloServer({
+    schema,
+    playground: true,
+  });
+  const {url} = await server.listen(PORT);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+}
+
+bootstrap().catch(console.error);
