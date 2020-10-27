bf0efe9c6694420f03ffe3bef21ee6ea84c629db
vikei <vikei@gmail.com>
2020-10-27 18:18:00 +0200

    Add update stock when create order

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -5,6 +5,7 @@ import OrderEntity from "./order.entity";
 import {In, Repository} from "typeorm/index";
 import {OrderInput} from "./orders.args";
 import ProductEntity from "../products/product.entity";
+import StockEntity from "../stocks/stock.entity";
 
 @Service()
 @Resolver(() => OrderEntity)
@@ -12,12 +13,22 @@ export default class OrdersResolver {
   constructor(
     @InjectRepository(OrderEntity) private orderRepository: Repository<OrderEntity>,
     @InjectRepository(ProductEntity) private productRepository: Repository<ProductEntity>,
+    @InjectRepository(StockEntity) private stockRepository: Repository<StockEntity>,
   ) {}
 
   @Mutation(() => OrderEntity)
   async createOrder(@Arg("input") input: OrderInput) {
     const order = await this.orderRepository.save(this.orderRepository.create(input));
-    // update stock
+
+    const productIds = order.inventory.map(inventory => inventory.productId);
+    const stocks = await this.stockRepository.find({where: {id: In(productIds)}});
+    const newStocksData = order.inventory.map(inventory => {
+      const stock = {...stocks.find(stock => stock.productId === inventory.productId)!};
+      stock.count = stock.count - inventory.count;
+      return stock;
+    });
+    await this.stockRepository.save(newStocksData);
+
     return order;
   }
 
