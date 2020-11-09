34e65c07a93b7fcaafa770466a93c20976879ef0
vikei <vikei@gmail.com>
2020-11-09 22:00:32 +0200

    Remove Entity decorator

--- src/stocks/gateway/stock.type.ts
+++ src/stocks/gateway/stock.type.ts
@@ -1,9 +1,7 @@
 import {Field, ID, Int, ObjectType} from "type-graphql";
-import {Entity} from "typeorm/index";
 import ProductType from "../../products/gateway/product.type";
 
 @ObjectType("Stock")
-@Entity()
 export default class StockType {
   @Field(() => ID)
   id: string;
